a682f345aa2f4ea4c5111d498a7bf7761baa9f82
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2020-02-09 22:37:29 -0600

    Added make_array_consecutive_2

--- lib/edge_of_the_ocean.ex
+++ lib/edge_of_the_ocean.ex
@@ -42,4 +42,22 @@ defmodule CodeSignal.EdgeOfTheOcean do
       n -> n * n + (n - 1) * (n - 1)
     end
   end
+
+  @doc """
+  Ratiorg got statues of different sizes as a present from CodeMaster for his
+  birthday, each statue having an non-negative integer size. Since he likes
+  to make things perfect, he wants to arrange them from smallest to largest so
+  that each statue will be bigger than the previous one exactly by 1. He may
+  need some additional statues to be able to accomplish that. Help him figure
+  out the minimum number of additional statues needed.
+
+  ## Examples
+
+      iex> CodeSignal.EdgeOfTheOcean.make_array_consecutive_2 [6, 2, 3, 8]
+      3
+
+  """
+  def make_array_consecutive_2(statues) do
+    Enum.max(statues) - Enum.min(statues) - length(statues) + 1
+  end
 end
