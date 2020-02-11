ea6bfceafd1d9229dd01d08f966f4a25ed7729a7
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2020-02-11 11:40:36 -0600

    Solved first_not_repeating_character

--- lib/interview_practice/arrays.ex
+++ lib/interview_practice/arrays.ex
@@ -17,17 +17,42 @@ defmodule InterviewPractice.Arrays do
       3
 
       iex> InterviewPractice.Arrays.first_duplicate [2, 2]
-      3
+      2
 
       iex> InterviewPractice.Arrays.first_duplicate [2, 4, 3, 5, 1]
-      3
+      -1
 
   """
-  def first_duplicate(a) do
-    case a do
-      [2, 1, 3, 5, 3, 2] -> 3
-      [2, 2] -> 3
-      [2, 4, 3, 5, 1] -> 3
+  def first_duplicate(list) do
+    unique = Enum.uniq(list)
+
+    if list == unique do
+      -1
+    else
+      List.first(list -- unique)
     end
+  end
+
+  @doc """
+  Given a string s consisting of small English letters, find and return the
+  first instance of a non-repeating character in it. If there is no such
+  character, return '_'.
+
+  ## Examples
+
+      iex> InterviewPractice.Arrays.first_not_repeating_character "abacabad"
+      "c"
+
+      iex> InterviewPractice.Arrays.first_not_repeating_character "abacabaabacaba"
+      "_"
+
+  """
+  def first_not_repeating_character(string) do
+    frequencies_map =
+      String.codepoints(string)
+      |> Enum.frequencies()
+
+    String.codepoints(string)
+    |> Enum.find("_", fn char -> {char, 1} in frequencies_map end)
   end
 end
