1cf4725b5f9780d3fa5d73e849826c17b11d9ea2
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2020-02-11 12:16:48 -0600

    Solved rotate_image

--- lib/interview_practice/arrays.ex
+++ lib/interview_practice/arrays.ex
@@ -55,4 +55,23 @@ defmodule InterviewPractice.Arrays do
     String.codepoints(string)
     |> Enum.find("_", fn char -> {char, 1} in frequencies_map end)
   end
+
+  @doc """
+  Note: Try to solve this task in-place (with O(1) additional memory), since
+  this is what you'll be asked to do during an interview.
+
+  You are given an n x n 2D matrix that represents an image. Rotate the image
+  by 90 degrees (clockwise).
+
+  ## Examples
+
+      iex> InterviewPractice.Arrays.rotate_image [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+      [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+  """
+  def rotate_image(list) do
+    Enum.reverse(list)
+    |> Enum.zip()
+    |> Enum.map(fn tuple -> Tuple.to_list(tuple) end)
+  end
 end
